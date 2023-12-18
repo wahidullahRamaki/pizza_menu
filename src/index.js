b1dd06f69cc6f3d2741ff0278ca4294155f34c34
wahidullahRamaki <30998117+wahidullahRamaki@users.noreply.github.com>
2023-12-18 10:53:33 +0430

    finish the project

--- src/index.js
+++ src/index.js
@@ -72,29 +72,32 @@ function Menue() {
   const pizzaNum = pizzas.length;
   return (
     <main className="menu">
+      <h1>Our Menue</h1>
       {pizzaNum > 0 ? (
-        <ul className="pizzas">
-          {pizzas.map((pizza) => (
-            <Pizza pizzaObj={pizza} key={pizza.name} />
-          ))}
-        </ul>
+        <>
+          <p>Italian 6 famouse Dashes</p>
+          <ul className="pizzas">
+            {pizzas.map((pizza) => (
+              <Pizza pizzaObj={pizza} key={pizza.name} />
+            ))}
+          </ul>
+        </>
       ) : (
         <p> We are stil working on our menue, Please come back :) </p>
       )}
-      <h1>Our Menue</h1>
     </main>
   );
 }
 
 function Pizza({ pizzaObj }) {
-  if (pizzaObj.soldOut) return null;
+  // if (pizzaObj.soldOut) return null;
   return (
-    <li className="pizza">
+    <li className={`pizza ${pizzaObj.soldOut ? "sold-out" : ""}`}>
       <img src={pizzaObj.photoName} alt={pizzaObj.name} />
       <div>
         <h3>{pizzaObj.name}</h3>
         <p>{pizzaObj.ingredients}</p>
-        <span>{pizzaObj.price}</span>
+        <span>{pizzaObj.soldOut ? " SOLD OUT" : pizzaObj.price}</span>
       </div>
     </li>
   );
